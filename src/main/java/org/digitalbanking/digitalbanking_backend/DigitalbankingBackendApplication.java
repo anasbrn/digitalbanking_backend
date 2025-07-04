6956fa65b708453c420c3030b2985e79648f07a4
BARNOCH Anas <anasbarnoch@gmail.com>
2025-07-04 16:25:55 +0100

    Init project

---
+++ src/main/java/org/digitalbanking/digitalbanking_backend/DigitalbankingBackendApplication.java
@@ -0,0 +1,13 @@
+package org.digitalbanking.digitalbanking_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalbankingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalbankingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/digitalbanking/digitalbanking_backend/DigitalbankingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.digitalbanking.digitalbanking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalbankingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
